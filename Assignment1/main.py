a87f5edac37e5c217914bb8b03f181a99f8a3719
Kaucek <kaucek2212@gmail.com>
2026-05-21 22:09:37 +0800

    Final edits

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -70,7 +70,31 @@ if not os.path.exists(bronze_lms_directory):
 for date_str in dates_str_lst:
     utils.data_processing_bronze_table.process_bronze_table(date_str, bronze_lms_directory, spark)
 
+bronze_feature_directory = "datamart/bronze/feature_clickstream/"
 
+if not os.path.exists(bronze_feature_directory):
+    os.makedirs(bronze_feature_directory)
+
+# run bronze backfill
+for date_str in dates_str_lst:
+    utils.data_processing_bronze_table.process_bronze_feature_table("feature_clickstream",date_str, bronze_feature_directory, spark)
+
+bronze_feature_directory = "datamart/bronze/features_attributes/"
+
+if not os.path.exists(bronze_feature_directory):
+    os.makedirs(bronze_feature_directory)
+    
+for date_str in dates_str_lst:
+    utils.data_processing_bronze_table.process_bronze_feature_table("features_attributes",date_str, bronze_feature_directory, spark)
+
+bronze_feature_directory = "datamart/bronze/features_financials/"
+
+if not os.path.exists(bronze_feature_directory):
+    os.makedirs(bronze_feature_directory)
+    
+for date_str in dates_str_lst:
+    utils.data_processing_bronze_table.process_bronze_feature_table("features_financials",date_str, bronze_feature_directory, spark)
+    
 # create silver datalake
 silver_loan_daily_directory = "datamart/silver/loan_daily/"
 
@@ -81,6 +105,34 @@ if not os.path.exists(silver_loan_daily_directory):
 for date_str in dates_str_lst:
     utils.data_processing_silver_table.process_silver_table(date_str, bronze_lms_directory, silver_loan_daily_directory, spark)
 
+# Silver Feature Attributes
+silver_features_attributes_directory = "datamart/silver/features_attributes/"
+bronze_features_directory = "datamart/bronze/features_attributes/"
+
+if not os.path.exists(silver_features_attributes_directory):
+    os.makedirs(silver_features_attributes_directory)
+for date_str in dates_str_lst:
+    utils.data_processing_silver_table.process_features_attributes_silver_table(date_str,  bronze_features_directory, silver_features_attributes_directory, spark)
+    
+# Silver Feature Financials
+silver_features_financials_directory = "datamart/silver/features_financials/"
+bronze_features_directory = "datamart/bronze/features_financials/"
+
+if not os.path.exists(silver_features_financials_directory):
+    os.makedirs(silver_features_financials_directory)
+
+for date_str in dates_str_lst:
+    utils.data_processing_silver_table.process_features_financials_silver_table(date_str,  bronze_features_directory, silver_features_financials_directory, spark)
+
+# Silver feature clickstream
+silver_feature_clickstream_directory = "datamart/silver/feature_clickstream/"
+bronze_features_directory = "datamart/bronze/feature_clickstream/"
+
+if not os.path.exists(silver_feature_clickstream_directory):
+    os.makedirs(silver_feature_clickstream_directory)
+
+for date_str in dates_str_lst:
+    utils.data_processing_silver_table.process_feature_clickstream_silver_table(date_str, bronze_features_directory, silver_feature_clickstream_directory, spark)
 
 # create gold datalake
 gold_label_store_directory = "datamart/gold/label_store/"
@@ -100,6 +152,17 @@ print("row_count:",df.count())
 
 df.show()
 
+#Gold Feature store
+silver_fa_directory="datamart/silver/features_attributes/"
+silver_ff_directory="datamart/silver/features_financials/"
+silver_fc_directory="datamart/silver/feature_clickstream/"
+
+gold_feature_store_directory = "datamart/gold/feature_store/"
+
+if not os.path.exists(gold_feature_store_directory):
+    os.makedirs(gold_feature_store_directory)
+
+for date_str in dates_str_lst:
+    utils.data_processing_gold_table.process_feature_gold_tables(date_str, silver_fa_directory, silver_ff_directory, silver_fc_directory, gold_feature_store_directory, spark)
 
 
-    
